174839570b47b8b8e5db36a6a76c5dcb2f3269e4
parkerstovall <parker.stovall@gmail.com>
2024-10-27 15:38:12 -0500

    gallery captions

--- pho4ever_website/src/components/galleryItem.tsx
+++ pho4ever_website/src/components/galleryItem.tsx
@@ -1,15 +1,35 @@
 import * as React from "react";
-import { Box, Grid2 as Grid } from "@mui/material";
+import { Box, Grid2 as Grid, Typography } from "@mui/material";
 import { Gallery_Item } from "../constants/gallery_items";
 
 export default function GalleryItem(item: Gallery_Item) {
   return (
-    <Grid size={{ xs: 12, sm: item.width }}>
+    <Grid
+      sx={{
+        "&:hover": { transform: "scale(1.1)" },
+        transition: ".3s ease all",
+        position: "relative",
+      }}
+      size={{ xs: 12, sm: item.width }}
+    >
       <Box
         sx={{ width: "100%", height: "400px", objectFit: "cover" }}
         component="img"
         src={item.img}
       />
+
+      <Box
+        component={"div"}
+        sx={{ transition: ".5s ease all" }}
+        className="hover:opacity-100 opacity-0 absolute inset-0"
+      >
+        <Typography
+          variant="h6"
+          className="w-full text-center bottom-2 absolute text-white [text-shadow:_0_0_8px_black]"
+        >
+          {item.caption}
+        </Typography>
+      </Box>
     </Grid>
   );
 }
