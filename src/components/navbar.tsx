47b31012344cf9eb132e33866414c88030fd62d0
parkerstovall <parker.stovall@gmail.com>
2024-10-28 08:45:57 -0500

    added navbar to container

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import {
   AppBar,
   Box,
+  Container,
   IconButton,
   Menu,
   MenuItem,
@@ -20,82 +21,84 @@ export default function NavBar() {
   return (
     <AppBar position='sticky'>
       <Toolbar className='bg-pho-navy'>
-        <Typography fontSize={{ xs: '1.75rem', sm: '2.125rem' }} variant='h4'>
-          Pho-4-Ever
-        </Typography>
-        <Box
-          height={{ xs: '40px', sm: '50px' }}
-          sx={{ marginLeft: { xs: '5px', sm: '15px' } }}
-          component='img'
-          src={`${APP_PATH}images/animatedBowlOfPho.webp`}
-        />
+        <Container maxWidth='md' className='flex items-center'>
+          <Typography fontSize={{ xs: '1.75rem', sm: '2.125rem' }} variant='h4'>
+            Pho-4-Ever
+          </Typography>
+          <Box
+            height={{ xs: '40px', sm: '50px' }}
+            sx={{ marginLeft: { xs: '5px', sm: '15px' } }}
+            component='img'
+            src={`${APP_PATH}images/animatedBowlOfPho.webp`}
+          />
 
-        <Stack
-          direction={'row'}
-          spacing={3}
-          sx={{ marginLeft: 'auto', display: { md: 'flex', xs: 'none' } }}
-        >
-          {pages.map((page) => {
-            return (
-              <Link key={page.title} href={page.href}>
-                {page.title}
-              </Link>
-            )
-          })}
-        </Stack>
-
-        <Box
-          ref={ref}
-          sx={{
-            flexShrink: 1,
-            marginLeft: 'auto',
-            display: { xs: 'flex', md: 'none' },
-          }}
-        >
-          <IconButton
-            size='large'
-            aria-label='account of current user'
-            aria-controls='menu-appbar'
-            aria-haspopup='true'
-            onClick={() => {
-              setMenuOpen(true)
-            }}
-            color='inherit'
+          <Stack
+            direction={'row'}
+            spacing={3}
+            sx={{ marginLeft: 'auto', display: { md: 'flex', xs: 'none' } }}
           >
-            <MenuIcon />
-          </IconButton>
-          <Menu
-            id='menu-appbar'
-            anchorEl={ref?.current}
-            anchorOrigin={{
-              vertical: 'bottom',
-              horizontal: 'left',
-            }}
-            keepMounted
-            transformOrigin={{
-              vertical: 'top',
-              horizontal: 'left',
-            }}
-            open={menuOpen}
-            onClose={() => {
-              setMenuOpen(false)
-            }}
-            sx={{ display: { xs: 'block', md: 'none' } }}
-          >
-            {pages.map((page) => (
-              <MenuItem
-                key={page.title}
-                onClick={() => {
-                  setMenuOpen(false)
-                }}
-              >
-                <Link href={page.href} style={{ textAlign: 'center' }}>
+            {pages.map((page) => {
+              return (
+                <Link key={page.title} href={page.href}>
                   {page.title}
                 </Link>
-              </MenuItem>
-            ))}
-          </Menu>
-        </Box>
+              )
+            })}
+          </Stack>
+
+          <Box
+            ref={ref}
+            sx={{
+              flexShrink: 1,
+              marginLeft: 'auto',
+              display: { xs: 'flex', md: 'none' },
+            }}
+          >
+            <IconButton
+              size='large'
+              aria-label='account of current user'
+              aria-controls='menu-appbar'
+              aria-haspopup='true'
+              onClick={() => {
+                setMenuOpen(true)
+              }}
+              color='inherit'
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id='menu-appbar'
+              anchorEl={ref?.current}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={menuOpen}
+              onClose={() => {
+                setMenuOpen(false)
+              }}
+              sx={{ display: { xs: 'block', md: 'none' } }}
+            >
+              {pages.map((page) => (
+                <MenuItem
+                  key={page.title}
+                  onClick={() => {
+                    setMenuOpen(false)
+                  }}
+                >
+                  <Link href={page.href} style={{ textAlign: 'center' }}>
+                    {page.title}
+                  </Link>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Container>
       </Toolbar>
     </AppBar>
   )
